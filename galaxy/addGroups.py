88aaa2982fa57bfe69a18749042e55b9ef0c0349
dpryan79 <dpryan79@gmail.com>
2017-03-31 09:46:23 +0200

    Copy over changes to 17.01. Note that automounting isn't yet being done.

---
+++ galaxy/addGroups.py
@@ -0,0 +1,11 @@
+#!/usr/bin/env python
+import os.path
+import subprocess
+
+if os.path.exists("/export/groups.tsv"):
+    f = open("/export/groups.tsv", "r")  # This has two columns: group, GID
+    for line in f:
+        cols = line.strip().split()
+        subprocess.check_call(["groupadd", "-r", cols[0], "-g", cols[1]])
+        subprocess.check_call(["gpasswd", "-a", "galaxy", cols[0]])
+    f.close()
